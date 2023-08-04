381ce8332a9ae61743f418820fc1ba4ccad464bd
lgarci78 <74846976+lgarci78@users.noreply.github.com>
2023-08-04 15:03:52 -0700

    Create Tree.cs

---
+++ Tree.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace BehaviorTree
+{
+    public abstract class Tree : MonoBehaviour
+    {
+        private Node root = null;
+
+        protected void Start()
+        {
+            root = SetupTree();     //Sets up behavior tree
+        }
+
+        private void Update()
+        {
+            if (root != null)
+                root.Evaluate();    // If it has a tree it will be evaluated continuosly 
+        }
+
+        protected abstract Node SetupTree();    // responsible for creating and configuring the nodes, should return the root node
+    }
+}
